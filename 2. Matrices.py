a39b87576f1211bb0c9dab94b86910374c80f370
keerthx <officialprakeerthjm@gmail.com>
2023-02-08 22:15:16 +0530

    Added 2. Matrices

---
+++ 2. Matrices.py
@@ -0,0 +1,7 @@
+#Matrices are an important part of Scientific Computation, they are used to plot graphs, simplify computation of linear equations and much more 
+# In this section we will study about Matrices using the numpy package
+
+import numpy as np 
+
+#Ways to create an array 
+a = np.array([[1,2,3],[4,5,6],[7,8,9]]) #This makes a 3x3 matrix with each [] representing a row
