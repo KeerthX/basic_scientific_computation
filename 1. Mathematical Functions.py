bbc794c9a05212542600700b02491bfbd93e5804
keerthx <officialprakeerthjm@gmail.com>
2023-02-08 22:10:13 +0530

    Trignometric Functions

--- 1. Mathematical Functions.py
+++ 1. Mathematical Functions.py
@@ -1,5 +1,7 @@
 #In this project we will learn about some basic math functions that is used in python for Scientific Computing
 
+#Complex Functions
+
 import cmath
 x = 1 #Any Number 
 y = 2 #Any Number 
@@ -12,3 +14,15 @@ print(img)
 absolute = abs(z) #Gives the absolute value of the complex numebr
 print(absolute)
 
+#Trignometric Functions
+
+import numpy as np 
+
+t = 0 #This is the angle in radians
+
+x = np.sin(t) #This returns the sine value
+print(x)
+y = np.cos(t) #This returns the consine value
+print(y)
+z = np.tan(t) #This returns the tan value
+print(z)
