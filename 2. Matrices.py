badc48f77887edf2e98c15f447504754f002a65b
keerthx <officialprakeerthjm@gmail.com>
2023-02-08 23:57:38 +0530

    Mathematical operations on Matrices

--- 2. Matrices.py
+++ 2. Matrices.py
@@ -5,3 +5,18 @@ import numpy as np
 
 #Ways to create an array 
 a = np.array([[1,2,3],[4,5,6],[7,8,9]]) #This makes a 3x3 matrix with each [] representing a row
+b = np.array([[1,1,1],[1,1,1],[1,1,1]]) #This makes a 3x3 matrix with each [] representing a row
+
+#Addition of Matrices
+c = a + b 
+print(c) # c automatically becomes a matrix 
+
+#Subtraction of Matrices
+c = a - b 
+print(c) # c automatically becomes a matrix 
+
+#Multiply of Matrices
+c = np.dot(a,b)
+d = a @ b # Using the "@" operation 
+print(c) # c automatically becomes a matrix 
+print(d) # d automatically becomes a matrix 
