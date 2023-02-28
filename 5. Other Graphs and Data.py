7ec000ad95f2db382fa014066404147ac963f994
keerthx <officialprakeerthjm@gmail.com>
2023-02-28 21:46:53 +0530

    Init

--- 5. Other Graphs and Data.py
+++ 5. Other Graphs and Data.py
@@ -1,6 +1,33 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-x = np.random.normal(170,5,250) #(mean,standard deviation, number of data points)
-plt.hist(x)
-plt.show()
+# x = np.random.normal(170,5,250) #(mean,standard deviation, number of data points)
+# plt.hist(x)
+# plt.show()
+
+
+import numpy as np
+
+# Define a rectangular matrix A
+A = np.array([[1, 1, 1], [1, 1, 1]])
+
+# Compute the SVD of A
+U, s, VT = np.linalg.svd(A)
+print(U)
+print(s)
+print(VT)
+print("-------------------------")
+# Reconstruct the original matrix
+S = np.zeros_like(A)
+print(S)
+print("-------------------------")
+S[:min(A.shape), :min(A.shape)] = np.diag(s)
+print(S)
+print("-------------------------")
+A_reconstructed = U.dot(S.dot(VT))
+print(A.shape)
+# Print the results
+print("-------------------------")
+print("Original matrix A =\n", A)
+print("Reconstructed matrix A_reconstructed =\n", A_reconstructed)
+
